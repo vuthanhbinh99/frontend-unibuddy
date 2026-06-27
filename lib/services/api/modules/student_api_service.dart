cf43a84d9f85cf41f8f6bab35b353fadc3912b78
vuthanhbinh99 <binhvuthanh483@gmail.com>
2026-06-27 21:04:55 +0700

    cài api cho FE Sinh viên

--- lib/services/api/modules/student_api_service.dart
+++ lib/services/api/modules/student_api_service.dart
@@ -1,4 +1,7 @@
 import '../../../models/auth_models.dart';
+import '../../../models/student_course_models.dart';
+import '../../../models/student_home_models.dart';
+import '../../../models/student_schedule_models.dart';
 import '../api_client.dart';
 
 class StudentApiService {
@@ -9,5 +12,26 @@ class StudentApiService {
   Future<PublicUser> getCurrentUser() async {
     final data = await _apiClient.get('/users/me');
     return PublicUser.fromJson(data as Map<String, dynamic>);
+  }
+
+  Future<StudentHomeData> getStudentHomeData() async {
+    final user = await getCurrentUser();
+    return StudentHomeData.fromCurrentUser(user);
+  }
+
+  Future<StudentScheduleData> listSchedules({String? maMonHoc}) async {
+    final data = await _apiClient.get(
+      '/schedules',
+      query: maMonHoc == null ? null : {'maMonHoc': maMonHoc},
+    );
+    return StudentScheduleData.fromJson(data);
+  }
+
+  Future<StudentCourseData> listCourses({String? maHocKy}) async {
+    final data = await _apiClient.get(
+      '/courses',
+      query: maHocKy == null ? null : {'maHocKy': maHocKy},
+    );
+    return StudentCourseData.fromJson(data);
   }
 }
