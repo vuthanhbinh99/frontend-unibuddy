b2adde8f46138d4bc85a3d62e72938aa5d61f822
vuthanhbinh99 <binhvuthanh483@gmail.com>
2026-08-03 23:28:42 +0700

    Cập nhật điểm khởi chạy ứng dụng

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
 import 'app.dart';
+import 'services/notifications/push_notification_service.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  FirebaseMessaging.onBackgroundMessage(firebaseMessagingBackgroundHandler);
   runApp(const UniBuddyApp());
 }
